1c30e650eab8f6b4931bba11443c1a028f7ac8bd
Gabouchet <gabriel.hamel@epitech.eu>
2020-01-15 14:23:14 +0100

    Cmakefile initialized

--- Sources/src/Network/BoostNetworkServer.cpp
+++ Sources/src/Network/BoostNetworkServer.cpp
@@ -3,7 +3,7 @@
 #include "BoostNetworkServer.hpp"
 
 zia::net::BoostNetworkServer::BoostNetworkServer(unsigned short port) :
-m_socket(m_io), m_acceptor(m_io, tcp::endpoint(tcp::v4(), port)), m_networkManager()
+m_socket(m_io), m_acceptor(m_io, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)), m_networkManager()
 {
     bindAcceptor();
 }
@@ -21,7 +21,7 @@ void zia::net::BoostNetworkServer::run()
 void zia::net::BoostNetworkServer::bindAcceptor()
 {
     boost::shared_ptr<zia::net::BoostNetworkClient> connection(new zia::net::BoostNetworkClient(this->m_acceptor, this->m_networkManager));
-    auto binding = boost::bind(&BoostNetworkServer::acceptHandler, this, connection, placeholders::error);
+    auto binding = boost::bind(&BoostNetworkServer::acceptHandler, this, connection, boost::asio::placeholders::error);
     this->m_acceptor.async_accept(connection->getSocket(), binding);
 }
 
