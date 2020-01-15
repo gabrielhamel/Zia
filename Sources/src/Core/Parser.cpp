90b06708e464861cc223e8f6660f468af86eb1e8
loic.bontemps <loic.bontemps@localhost.localdomain>
2020-01-15 15:50:26 +0100

    Added HttpRequest.hpp into Parser

--- Sources/src/Core/Parser.cpp
+++ Sources/src/Core/Parser.cpp
@@ -1,6 +1,8 @@
+#include <iostream>
 #include "Parser.hpp"
+#include "HttpRequest.hpp"
 
 void zia::Parser::parse(std::shared_ptr<zia::net::User> user, std::string text)
 {
-
+    HttpRequest request = HttpRequest(text);
 }
