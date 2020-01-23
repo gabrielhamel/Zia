9b4e47dad2f3bcd7e9a35a8f5491982a29b05bf2
Gabouchet <gabriel.hamel@epitech.eu>
2020-01-23 17:26:49 +0100

    Move yml example in Configurations class

--- Sources/src/Core/Configurations.cpp
+++ Sources/src/Core/Configurations.cpp
@@ -30,4 +30,47 @@ void core::Configurations::updatePath(const std::string &filename)
 void core::Configurations::reload()
 {
     // Load configs
+    // #include "yconf/ConfigNode.hpp"
+    // #include "yconf/Helper.hpp"
+
+    /**
+     * @brief Simple yaml config file parsing
+     * Test file used :
+     *
+
+    martin:
+    name: Martin D'vloper
+    job: Developer
+    age: 22
+        job: Baker
+    experience:
+        duration: 2
+    skills:
+        - python
+        - perl
+        - pascal
+    */
+
+    // void test_config()
+    // {
+    //     try {
+    //         std::unique_ptr<IConfigNode> config = std::make_unique<yconf::ConfigNode>("../../sample.yaml");
+    //         config = config->getChild("martin");
+
+
+    //         std::cout << "Config : martin age is " << yconf::helper::getAs<int>(*config, "age") << std::endl;
+    //         std::cout << "Config : martin name is " << config->getValue("name") << std::endl;
+    //         std::cout << "Config : martin.experience.job " << config->getValue("experience.job") << std::endl;
+    //         std::cout << "Config : martin.experience.duration " << yconf::helper::getAs<int>(*config, "experience.duration") << std::endl;
+
+    //         std::cout << "Config : martin.skills content is [ " << std::endl;
+    //         for (const auto &field : config->getArray("skills"))
+    //                 std::cout << "'" << field << "'" << std::endl;
+    //         std::cout << "]" << std::endl;
+
+
+    //     } catch (std::exception const &e) {
+    //         std::cerr << "Error while testing config nodes : " << e.what() << std::endl;
+    //     }
+    // }
 }
--- Sources/src/main.cpp
+++ Sources/src/main.cpp
@@ -2,49 +2,6 @@
 #include "ListenersControl.hpp"
 #include "CmdLine.hpp"
 #include "Configurations.hpp"
-#include "yconf/ConfigNode.hpp"
-#include "yconf/Helper.hpp"
-
-/**
- * @brief Simple yaml config file parsing
- * Test file used :
- *
-
-martin:
-  name: Martin D'vloper
-  job: Developer
-  age: 22
-    job: Baker
-  experience:
-    duration: 2
-  skills:
-    - python
-    - perl
-    - pascal
- */
-
-void test_config()
-{
-    try {
-        std::unique_ptr<IConfigNode> config = std::make_unique<yconf::ConfigNode>("../../sample.yaml");
-        config = config->getChild("martin");
-
-
-        std::cout << "Config : martin age is " << yconf::helper::getAs<int>(*config, "age") << std::endl;
-        std::cout << "Config : martin name is " << config->getValue("name") << std::endl;
-        std::cout << "Config : martin.experience.job " << config->getValue("experience.job") << std::endl;
-        std::cout << "Config : martin.experience.duration " << yconf::helper::getAs<int>(*config, "experience.duration") << std::endl;
-
-        std::cout << "Config : martin.skills content is [ " << std::endl;
-        for (const auto &field : config->getArray("skills"))
-                std::cout << "'" << field << "'" << std::endl;
-        std::cout << "]" << std::endl;
-
-
-    } catch (std::exception const &e) {
-        std::cerr << "Error while testing config nodes : " << e.what() << std::endl;
-    }
-}
 
 int main(int ac, char **av)
 {
